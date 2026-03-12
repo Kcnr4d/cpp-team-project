7e60b1253707eef6b601ea665d851325fd027ba3
troj231 <kuba.2005.szlachetka.pl@gmail.com>
2026-03-12 12:15:10 +0100

    Dodane funcje odejmowania i nazwisko B

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
-
+int add(int a, int b) { 
+   return a + b; 
+} 
 int main() {
   cout << "Witaj w naszym programie!" << endl;
   return 0;
@@ -9,5 +11,6 @@ using namespace std;
 
 int main() {
   cout << "Witaj w naszym programie!" << endl;
-  return 0;
+  cout << "2 + 3 = " << add(2, 3) << endl;  
+return 0;
 }
