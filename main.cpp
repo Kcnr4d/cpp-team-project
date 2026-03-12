255d49b5d0aec41556ab36f72f17736d580003b2
Konrad Rzeminski <krzeminski@student.agh.edu.pl>
2026-03-12 11:40:25 +0100

    Inicjalizacja projektu

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Witaj w naszym programie!" << endl;
+  return 0;
+} #include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Witaj w naszym programie!" << endl;
+  return 0;
+}
